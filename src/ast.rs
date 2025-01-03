1383b439e947e1ab7fc74c45903a25ca336d6951
David Moreno <dmoreno@coralbits.com>
2025-01-03 22:33:49 +0100

    Improvements on the query language

--- src/ast.rs
+++ src/ast.rs
@@ -17,10 +17,19 @@ pub enum AST {
 }
 
 pub fn parse(input: &str) -> Result<AST, String> {
-    let mut tokens = tokenize(input);
-    parse_expression(&mut tokens)
+    let mut tokens = match tokenize(input) {
+        Ok(tokens) => tokens,
+        Err(e) => return Err(e),
+    };
+    // We convert simple variables to strings. If want the variable as it must exist, use !!var
+    match parse_expression(&mut tokens) {
+        Ok(AST::Variable(var)) => Ok(AST::String(var)),
+        Ok(ast) => Ok(ast),
+        Err(e) => Err(e),
+    }
 }
 
+#[derive(Debug, PartialEq)]
 enum Token {
     Number(i64),
     Variable(String),
@@ -36,7 +45,7 @@ enum Token {
     Or,
 }
 
-fn tokenize(input: &str) -> Vec<Token> {
+fn tokenize(input: &str) -> Result<Vec<Token>, String> {
     let mut tokens = Vec::new();
     let mut chars = input.chars().peekable();
     while let Some(c) = chars.next() {
@@ -76,13 +85,29 @@ fn tokenize(input: &str) -> Vec<Token> {
                 tokens.push(Token::String(string));
             }
             '>' => {
-                tokens.push(Token::GreaterThan);
+                // >=
+                if let Some(&'=') = chars.peek() {
+                    chars.next();
+                    tokens.push(Token::GreaterThan);
+                } else {
+                    tokens.push(Token::Greater);
+                }
             }
             '<' => {
-                tokens.push(Token::LessThan);
+                // <=
+                if let Some(&'=') = chars.peek() {
+                    chars.next();
+                    tokens.push(Token::LessThan);
+                } else {
+                    tokens.push(Token::Less);
+                }
             }
             '=' => {
                 tokens.push(Token::Equal);
+                // optional =
+                if let Some(&'=') = chars.peek() {
+                    chars.next();
+                }
             }
             '!' => {
                 tokens.push(Token::Not);
@@ -93,19 +118,31 @@ fn tokenize(input: &str) -> Vec<Token> {
             '|' => {
                 tokens.push(Token::Or);
             }
-            _ => {}
+            ' ' => {}
+            _ => {
+                return Err(format!("unexpected character {:?}", c));
+            }
         }
     }
-    tokens
+    Ok(tokens)
 }
 
 fn parse_expression(tokens: &mut Vec<Token>) -> Result<AST, String> {
     let mut ast = parse_term(tokens)?;
+
     while let Some(token) = tokens.first() {
         match token {
+            Token::Greater => {
+                tokens.remove(0);
+                ast = AST::Greater(Box::new(ast), Box::new(parse_term(tokens)?));
+            }
             Token::GreaterThan => {
                 tokens.remove(0);
                 ast = AST::GreaterThan(Box::new(ast), Box::new(parse_term(tokens)?));
+            }
+            Token::Less => {
+                tokens.remove(0);
+                ast = AST::Less(Box::new(ast), Box::new(parse_term(tokens)?));
             }
             Token::LessThan => {
                 tokens.remove(0);
@@ -127,6 +164,18 @@ fn parse_expression(tokens: &mut Vec<Token>) -> Result<AST, String> {
                 tokens.remove(0);
                 ast = AST::Or(Box::new(ast), Box::new(parse_term(tokens)?));
             }
+            Token::Number(n) => {
+                ast = AST::Number(*n);
+                tokens.remove(0);
+            }
+            Token::Variable(v) => {
+                ast = AST::Variable(v.clone());
+                tokens.remove(0);
+            }
+            Token::String(s) => {
+                ast = AST::String(s.clone());
+                tokens.remove(0);
+            }
             _ => break,
         }
     }
@@ -142,7 +191,7 @@ fn parse_term(tokens: &mut Vec<Token>) -> Result<AST, String> {
         Token::Number(n) => Ok(AST::Number(n)),
         Token::Variable(v) => Ok(AST::Variable(v)),
         Token::String(s) => Ok(AST::String(s)),
-        _ => Err("unexpected token".to_string()),
+        token => Err(format!("unexpected token {:?}", token)),
     }
 }
 
@@ -153,8 +202,27 @@ enum Value {
     Boolean(bool),
 }
 
-pub fn execute(ast: AST, record: &Record) -> Value {
+/**
+ * Example cases, for record "this is a test=10", {"test": "10"}:
+ *
+ * "this" -> true
+ * this -> true
+ * !!this -> false
+ * test -> true
+ * !!test -> true
+ * !!nope -> false
+ * test >= 10 -> false
+ * 10 -> true
+ * 20 -> false
+ * test > 0 && test < 100 -> true
+ * test > 10 || test < 100 -> true
+ * test > 10 || test < 5 -> false
+ * (test > 10 || test < 5) && test < 100 -> false
+ */
+
+pub fn execute(ast: &AST, record: &Record) -> Value {
     match ast {
+        AST::String(s) => Value::Boolean(record.original.contains(s)),
         AST::Variable(var) => {
             if let Some(value) = record.get(&var) {
                 match value.parse::<i64>() {
@@ -162,66 +230,66 @@ pub fn execute(ast: AST, record: &Record) -> Value {
                     Err(_) => Value::String(value.to_string()),
                 }
             } else {
-                Value::String("".to_string())
+                Value::Boolean(false)
             }
         }
-        AST::String(s) => Value::String(s),
-        AST::Number(n) => Value::Number(n),
+        AST::Number(n) => Value::Number(n.clone()),
         AST::GreaterThan(lhs, rhs) => {
-            let lhs = execute(*lhs, record);
-            let rhs = execute(*rhs, record);
+            let lhs = execute(&lhs, record);
+            let rhs = execute(&rhs, record);
             match (lhs, rhs) {
                 (Value::Number(lhs), Value::Number(rhs)) => Value::Boolean(lhs > rhs),
                 _ => Value::Boolean(false),
             }
         }
         AST::LessThan(lhs, rhs) => {
-            let lhs = execute(*lhs, record);
-            let rhs = execute(*rhs, record);
+            let lhs = execute(&lhs, record);
+            let rhs = execute(&rhs, record);
             match (lhs, rhs) {
                 (Value::Number(lhs), Value::Number(rhs)) => Value::Boolean(lhs < rhs),
                 _ => Value::Boolean(false),
             }
         }
         AST::Equal(lhs, rhs) => {
-            let lhs = execute(*lhs, record);
-            let rhs = execute(*rhs, record);
+            let lhs = execute(&lhs, record);
+            let rhs = execute(&rhs, record);
             Value::Boolean(lhs == rhs)
         }
         AST::Greater(lhs, rhs) => {
-            let lhs = execute(*lhs, record);
-            let rhs = execute(*rhs, record);
+            let lhs = execute(&lhs, record);
+            let rhs = execute(&rhs, record);
             match (lhs, rhs) {
                 (Value::Number(lhs), Value::Number(rhs)) => Value::Boolean(lhs >= rhs),
                 _ => Value::Boolean(false),
             }
         }
         AST::Less(lhs, rhs) => {
-            let lhs = execute(*lhs, record);
-            let rhs = execute(*rhs, record);
+            let lhs = execute(&lhs, record);
+            let rhs = execute(&rhs, record);
             match (lhs, rhs) {
                 (Value::Number(lhs), Value::Number(rhs)) => Value::Boolean(lhs <= rhs),
                 _ => Value::Boolean(false),
             }
         }
         AST::Not(ast) => {
-            let ast = execute(*ast, record);
+            let ast = execute_to_bool(ast, record);
             match ast {
                 Value::Boolean(b) => Value::Boolean(!b),
                 _ => Value::Boolean(false),
             }
         }
         AST::And(lhs, rhs) => {
-            let lhs = execute(*lhs, record);
-            let rhs = execute(*rhs, record);
+            let lhs = execute_to_bool(&lhs, record);
+            let rhs = execute_to_bool(&rhs, record);
+
             match (lhs, rhs) {
                 (Value::Boolean(lhs), Value::Boolean(rhs)) => Value::Boolean(lhs && rhs),
                 _ => Value::Boolean(false),
             }
         }
         AST::Or(lhs, rhs) => {
-            let lhs = execute(*lhs, record);
-            let rhs = execute(*rhs, record);
+            let lhs = execute_to_bool(&lhs, record);
+            let rhs = execute_to_bool(&rhs, record);
             match (lhs, rhs) {
                 (Value::Boolean(lhs), Value::Boolean(rhs)) => Value::Boolean(lhs || rhs),
                 _ => Value::Boolean(false),
@@ -230,10 +298,17 @@ pub fn execute(ast: AST, record: &Record) -> Value {
         AST::Empty => Value::Boolean(true),
     }
 }
+fn execute_to_bool(ast: &AST, record: &Record) -> Value {
+    match execute(ast, record) {
+        Value::Number(n) => Value::Boolean(true),
+        Value::String(s) => Value::Boolean(record.data.get(&s).is_some()),
+        Value::Boolean(b) => Value::Boolean(b),
+    }
+}
 
 impl AST {
     pub fn matches(&self, record: &Record) -> bool {
-        match execute(self.clone(), record) {
+        match execute(self, record) {
             Value::Boolean(b) => b,
             _ => false,
         }
@@ -247,17 +322,55 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_tokenize() {
+        let tokens = tokenize("1 > 2");
+        assert_eq!(
+            tokens,
+            Ok(vec![Token::Number(1), Token::Greater, Token::Number(2)])
+        );
+        let tokens = tokenize("1 <= 2");
+        assert_eq!(
+            tokens,
+            Ok(vec![Token::Number(1), Token::LessThan, Token::Number(2)])
+        );
+        let tokens = tokenize("1 == 2");
+        assert_eq!(
+            tokens,
+            Ok(vec![Token::Number(1), Token::Equal, Token::Number(2)])
+        );
+        let tokens = tokenize("var >= 2");
+        assert_eq!(
+            tokens,
+            Ok(vec![
+                Token::Variable("var".to_string()),
+                Token::GreaterThan,
+                Token::Number(2)
+            ])
+        );
+        let tokens = tokenize("1 < \"string\"");
+        assert_eq!(
+            tokens,
+            Ok(vec![
+                Token::Number(1),
+                Token::Less,
+                Token::String("string".to_string())
+            ])
+        );
+    }
+
+    #[test]
     fn test_parse() {
+        assert_eq!(parse("1 >> 2").is_err(), true);
         assert_eq!(
             parse("1 > 2"),
-            Ok(AST::GreaterThan(
+            Ok(AST::Greater(
                 Box::new(AST::Number(1)),
                 Box::new(AST::Number(2))
             ))
         );
         assert_eq!(
             parse("1 < 2"),
-            Ok(AST::LessThan(
+            Ok(AST::Less(
                 Box::new(AST::Number(1)),
                 Box::new(AST::Number(2))
             ))
@@ -307,14 +420,25 @@ mod tests {
             )),
         );
     }
+
+    #[test]
+    fn test_one_var_is_string() {
+        assert_eq!(parse("var"), Ok(AST::String("var".into())));
+        assert_eq!(parse("\"var\""), Ok(AST::String("var".into())));
+        // assert_eq!(parse("!!var"), Ok(AST::Variable("var".into())));
+    }
+
+    #[test]
     fn test_execute() {
         let record = Record::new("2024-01-01 00:00:00".to_string())
             .set_data("hostname", "localhost".to_string())
             .set_data("program", "test".to_string())
-            .set_data("rest", "message".to_string());
+            .set_data("rest", "message".to_string())
+            .set_data("var1", "10".to_string())
+            .set_data("var2", "20".to_string());
         assert_eq!(
             execute(
-                AST::Equal(
+                &AST::Equal(
                     Box::new(AST::Variable("var1".to_string())),
                     Box::new(AST::Variable("var2".to_string())),
                 ),
@@ -324,7 +448,7 @@ mod tests {
         );
         assert_eq!(
             execute(
-                AST::GreaterThan(
+                &AST::GreaterThan(
                     Box::new(AST::Variable("var1".to_string())),
                     Box::new(AST::Variable("var2".to_string())),
                 ),
@@ -334,7 +458,32 @@ mod tests {
         );
         assert_eq!(
             execute(
-                AST::LessThan(
+                &AST::LessThan(
+                    Box::new(AST::Variable("var1".to_string())),
+                    Box::new(AST::Variable("var2".to_string())),
+                ),
+                &record,
+            ),
+            Value::Boolean(true),
+        );
+        assert_eq!(
+            execute(&AST::Variable("var1".to_string()), &record),
+            Value::Number(10)
+        );
+        assert_eq!(
+            execute(&AST::Variable("var2".to_string()), &record),
+            Value::Number(20)
+        );
+        assert_eq!(
+            execute(
+                &AST::Not(Box::new(AST::Variable("var2".to_string()))),
+                &record
+            ),
+            Value::Boolean(false)
+        );
+        assert_eq!(
+            execute(
+                &AST::And(
                     Box::new(AST::Variable("var1".to_string())),
                     Box::new(AST::Variable("var2".to_string())),
                 ),
@@ -344,17 +493,7 @@ mod tests {
         );
         assert_eq!(
             execute(
-                AST::And(
-                    Box::new(AST::Variable("var1".to_string())),
-                    Box::new(AST::Variable("var2".to_string())),
-                ),
-                &record,
-            ),
-            Value::Boolean(true),
-        );
-        assert_eq!(
-            execute(
-                AST::Or(
+                &AST::Or(
                     Box::new(AST::Variable("var1".to_string())),
                     Box::new(AST::Variable("var2".to_string())),
                 ),
